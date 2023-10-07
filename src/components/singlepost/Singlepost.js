8c47f394d41b1f5ae09a41ab855aec4e7ba95de3
Satya Prakash tiwari <93290682+satyaprakash-web@users.noreply.github.com>
2023-10-07 20:03:59 +0530

    Update Singlepost.js

--- src/components/singlepost/Singlepost.js
+++ src/components/singlepost/Singlepost.js
@@ -1,29 +1,35 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { useLocation, Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './singlepost.css';
-import UserContext from '../../context/UserContext';
-import { motion } from 'framer-motion';
+import React, { useEffect, useState, useContext } from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./singlepost.css";
+import UserContext from "../../context/UserContext";
+import { motion } from "framer-motion";
 
 function Singlepost() {
-
   //1 - get id from /post/:postId and render post with that id
   let location = useLocation();
   location = location.pathname;
-  const postId = location.split('/')[2];
+  const postId = location.split("/")[2];
   // console.log(postId);
 
   const [singlePost, setSinglePost] = useState({});
+  const [title, setTitle] = useState("");
+  const [description, setDescripion] = useState("");
 
   useEffect(() => {
     const fetchSinglePost = async () => {
       setProgress(0);
-      const res = await axios.get(`${process.env.REACT_APP_SERVER}/api/posts/${postId}`);
+      const res = await axios.get(
+        `${process.env.REACT_APP_SERVER}/api/posts/${postId}`
+      );
       // console.log(res);
       setProgress(30);
       setSinglePost(res.data);
+      setTitle(res.data.title);
+      console.log(res.data);
+      setDescripion(res.data.desc);
       setProgress(100);
-    }
+    };
 
     fetchSinglePost();
     // eslint-disable-next-line
@@ -33,27 +39,44 @@ function Singlepost() {
   // make Public folder
   const PF = `${process.env.REACT_APP_SERVER}/images/`;
 
-
   //2 - delete post if this post belongs to current user
   const context = useContext(UserContext);
   const { user, setProgress, mode } = context;
+  const [editVisible, setEditVisible] = useState(false);
   const navigate = useNavigate();
 
   const handleDelete = async () => {
     try {
-      const res = await axios.delete(`${process.env.REACT_APP_SERVER}/api/posts/delete/${postId}`, {
-        data: { username: user.username }
-      });
+      const res = await axios.delete(
+        `${process.env.REACT_APP_SERVER}/api/posts/delete/${postId}`,
+        {
+          data: { username: user.username },
+        }
+      );
       // go to homepage after deleting
       console.log(res.data);
-      navigate('/');
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
+  const toggleEditView = () => {
+    setEditVisible((prev) => !prev);
+  };
   // 3 - edit post if this post belongs to current user
 
+  const handlePostUpdate = () => {
+    axios.patch(`${process.env.REACT_APP_SERVER}/api/posts/update/${postId}`, {
+      data: { username: user.username, title: title, desc: description
+       },
+    }).then(res=>{
+      console.log(res);
+      window.location.reload();
+    }).catch(err=>{
+      console.log(err);
+    })
+  };
 
   return (
     <motion.div
@@ -62,35 +85,70 @@ function Singlepost() {
       className={`singlepost container-fluid theme-${mode}`}
     >
       <div className="singlepostWrapper">
-        <img src={singlePost.photo ? PF + singlePost.photo : "https://c4.wallpaperflare.com/wallpaper/405/400/980/winter-snow-sun-light-forest-trees-sunset-wallpaper-preview.jpg"} alt="img" className="singlepostImg" />
+        <img
+          src={
+            singlePost.photo
+              ? PF + singlePost.photo
+              : "https://c4.wallpaperflare.com/wallpaper/405/400/980/winter-snow-sun-light-forest-trees-sunset-wallpaper-preview.jpg"
+          }
+          alt="img"
+          className="singlepostImg"
+        />
 
-        <h1 className='singlepostTitle'>
+        <h1 className="singlepostTitle">
           {singlePost.title}
           {/* show delete and edit post icon if this post belongs to current user */}
-          {user.username === singlePost.username &&
+          {user.username === singlePost.username && (
             <div className="singlepostEdit">
-              <i className="singlepostIcon fa-regular fa-pen-to-square"></i>
-              <i className="singlepostIcon fa-regular fa-trash-can" onClick={handleDelete}></i>
+              <i
+                className="singlepostIcon fa-regular fa-pen-to-square"
+                onClick={toggleEditView}
+              ></i>
+              <i
+                className="singlepostIcon fa-regular fa-trash-can"
+                onClick={handleDelete}
+              ></i>
             </div>
-          }
+          )}
         </h1>
 
         <div className="singlepostInfo">
-          <span className='singlepostAuthor'>Author:<b>
-            {/* user specific posts ?use=john */}
-            <Link className='link' to={`/?user=${singlePost.username}`}>
-              {singlePost.username}
-            </Link>
-          </b></span>
-          <span className='singlepostDate'>{new Date(singlePost.createdAt).toDateString()}</span>
+          <span className="singlepostAuthor">
+            Author:
+            <b>
+              {/* user specific posts ?use=john */}
+              <Link className="link" to={`/?user=${singlePost.username}`}>
+                {singlePost.username}
+              </Link>
+            </b>
+          </span>
+          <span className="singlepostDate">
+            {new Date(singlePost.createdAt).toDateString()}
+          </span>
         </div>
 
-        <p className="singlepostDesc">
-          {singlePost.desc}
-        </p>
+        {editVisible && (
+          <div>
+            <div>
+              <input type="text" placeholder="Title" value={title} onChange={event=> setTitle(event.target.value)}/>
+            </div>
+            <div>
+              <textarea
+                placeholder="Description"
+                value={description}
+                onChange={event=> setDescripion(event.target.value)}
+              ></textarea>
+            </div>
+            <div>
+              <button onClick={handlePostUpdate}>Update</button>
+            </div>
+          </div>
+        )}
+
+        {!editVisible && <p className="singlepostDesc">{singlePost.desc}</p>}
       </div>
     </motion.div>
-  )
+  );
 }
 
 export default Singlepost;
